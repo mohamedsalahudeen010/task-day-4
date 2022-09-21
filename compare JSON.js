a4a2625664496c18de514ff7c88531de60df7c39
Mohamed Salahudeen <110691033+mohamedsalahudeen010@users.noreply.github.com>
2022-09-21 19:05:00 +0530

    Update compare JSON.js

--- compare JSON.js
+++ compare JSON.js
@@ -1,19 +1,4 @@
-// Getting input via STDIN
-const readline = require("readline");
-
-const inp = readline.createInterface({
-  input: process.stdin
-});
-
-const userInput = [];
-
-inp.on("line", (data) => {
-  userInput.push(data);
-});
-
-inp.on("close", () => {
-  //start-here
-  var obj1={
+ var obj1={
       "name":"person 1",
       "age": 5
     };
@@ -22,22 +7,27 @@ inp.on("close", () => {
       "name":"person 1"
      };
      function compareJSON(obj1,obj2){
-         if (JSON.stringify(obj1)!=JSON.stringify(obj2)){
-       console.log (false);
+         if (JSON.stringify(obj1)===JSON.stringify(obj2)){
+       console.log (true);
    }
-       else{ console.log(true)
+       else{ console.log(false);
        }
-       return result
+       return result;
      }
-     
-     compareJSON(obj1,obj2);
-     
-      
-
-   
- });
+     console.log(compareJSON(obj1,obj2));
 
 
 
-
-
+                                  <<<<<<<<<<<<<<<<<<<<<<<<<<<==========OR==========>>>>>>>>>>>>>>>>>>>>>>>>>>
+                                    
+   var obj1={
+      "name":"person 1",
+      "age": 5
+    };
+    var obj2={
+        "age": 5,
+      "name":"person 1"
+     };                                 
+  console.log(JSON.stringify(obj1)===JSON.stringify(obj2));                                  
+                                    
+                                    
